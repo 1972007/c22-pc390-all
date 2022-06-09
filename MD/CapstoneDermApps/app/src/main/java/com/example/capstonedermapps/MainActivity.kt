7758c02a1cdcc5cedfc4b15439cb081dad48ba7e
Sabrinarzq <99380156+Sabrinarzq@users.noreply.github.com>
2022-06-09 10:51:43 +0700

    update

---
+++ MD/CapstoneDermApps/app/src/main/java/com/example/capstonedermapps/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.capstonedermapps
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.example.capstonedermapps.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+
+        activityMainBinding.cardScan.setOnClickListener{
+            startActivity(Intent(this, ScanActivity::class.java))
+        }
+
+        activityMainBinding.cardWiki.setOnClickListener{
+            startActivity(Intent(this, SkinWikiActivity::class.java))
+        }
+        activityMainBinding.card.setOnClickListener{
+            startActivity(Intent(this, AboutUsActivity::class.java))
+        }
+
+        activityMainBinding.vLogout.setOnClickListener {
+            logout()
+            AlertDialog.Builder(this).apply {
+                setMessage(getString(R.string.log_out_success))
+                setPositiveButton(getString(R.string.continue_)) { _, _ ->
+                    startActivity(Intent(this@MainActivity, SignInActivity::class.java))
+                    finish()
+                }
+                create()
+                show()
+    }
+}
